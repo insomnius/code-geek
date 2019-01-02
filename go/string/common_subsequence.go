092d717095f97800f379e8883bb03fe19b1dac6e
insomnius <aliana.arib@gmail.com>
2019-01-02 08:09:16 +0700

    Add a little bit documentation

--- go/string/common_subsequence.go
+++ go/string/common_subsequence.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+/*
+The longest common subsequence (LCS) problem is the problem of finding the longest subsequence common
+to all sequences in a set of sequences (often just two sequences).
+
+source: https://en.wikipedia.org/wiki/Longest_common_subsequence_problem
+
+You are given 2 string and you should return the longest common subsequence word of it.
+Level: Medium
+*/
 func common_subsequence() {
 	fmt.Println("common subsequence")
 
@@ -11,6 +20,24 @@ func common_subsequence() {
 
 	string_1 = "VAIXBKNDFLDMORKQUBJP"
 	string_2 = "XGRKPROQISGKLNMWPUQO"
+	memoize = map[int]map[int]string{}
+	fmt.Printf("first string: %s, second string: %s.\nThe common subsequence is: %s\n\n",
+		string_1, string_2, common_subsequence_(string_1, string_2, 0, 0, memoize))
+
+	string_1 = "apple"
+	string_2 = "banana"
+	memoize = map[int]map[int]string{}
+	fmt.Printf("first string: %s, second string: %s.\nThe common subsequence is: %s\n\n",
+		string_1, string_2, common_subsequence_(string_1, string_2, 0, 0, memoize))
+
+	string_1 = "velociraptor"
+	string_2 = "marsupilami"
+	memoize = map[int]map[int]string{}
+	fmt.Printf("first string: %s, second string: %s.\nThe common subsequence is: %s\n\n",
+		string_1, string_2, common_subsequence_(string_1, string_2, 0, 0, memoize))
+
+	string_1 = "i love you"
+	string_2 = "i hate you"
 	memoize = map[int]map[int]string{}
 	fmt.Printf("first string: %s, second string: %s.\nThe common subsequence is: %s\n\n",
 		string_1, string_2, common_subsequence_(string_1, string_2, 0, 0, memoize))
