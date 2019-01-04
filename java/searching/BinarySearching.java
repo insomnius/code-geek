42e6b7b4650d4f78ed956807dee88dcc00b64f62
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-01-05 00:48:36 +0700

    Add information code for BinarySearching

--- java/searching/BinarySearching.java
+++ java/searching/BinarySearching.java
@@ -1,5 +1,39 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
+/**
+ * In computer science, binary search, also known as half-interval search,
+ * logarithmic search, or binary chop, is a search algorithm that finds
+ * the position of a target value within a sorted array. Binary search
+ * compares the target value to the middle element of the array. If
+ * they are not equal, the half in which the target cannot lie is eliminated
+ * and the search continues on the remaining half, again taking the middle 
+ * element to compare to the target value, and repeating this until the 
+ * target value is found. If the search ends with the remaining half being
+ * empty, the target is not in the array. Even though the idea is simple,
+ * implementing binary search correctly requires attention to some subtleties
+ * about its exit conditions and midpoint calculation, particularly if the
+ * values in the array are not all of the whole numbers in the range.
+ *
+ * Binary search runs in logarithmic time in the worst case, making O(log n)
+ * comparisons, where n is the number of elements in the array, the O is
+ * Big O notation, and log is the logarithm. Binary search takes constant
+ * (O(1)) space, meaning that the space taken by the algorithm is the same
+ * for any number of elements in the array. Binary search is faster than
+ * linear search except for small arrays, but the array must be sorted first.
+ * Although specialized data structures designed for fast searching, such as
+ * hash tables, can be searched more efficiently, binary search applies to
+ * a wider range of problems.
+ *
+ * There are numerous variations of binary search. In particular, fractional
+ * cascading speeds up binary searches for the same value in multiple arrays.
+ * Fractional cascading efficiently solves a number of search problems in 
+ * computational geometry and in numerous other fields. Exponential search
+ * extends binary search to unbounded lists. The binary search tree and B-tree
+ * data structures are based on binary search.
+ * https://en.wikipedia.org/wiki/Binary_search_algorithm
+ * Level: Medium
+ */
 class BinarySearching {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
