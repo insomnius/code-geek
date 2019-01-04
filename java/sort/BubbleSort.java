c10c3409b3ff8a3168bc871235190617a1c14919
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-01-05 00:14:33 +0700

    Add information code for BubbleSort

--- java/sort/BubbleSort.java
+++ java/sort/BubbleSort.java
@@ -1,6 +1,16 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
+/**
+ * Bubble sort, sometimes referred to as sinking sort, is a simple sorting algorithm that repeatedly
+ * steps through the list, compares adjacent pairs and swaps them if they are in the wrong order.
+ * The pass through the list is repeated until the list is sorted. The algorithm, which is a comparison
+ * sort, is named for the way smaller or larger elements "bubble" to the top of the list. Although the 
+ * algorithm is simple, it is too slow and impractical for most problems even when compared to insertion sort.
+ * Bubble sort can be practical if the input is mostly sorted order with some out-of-order.
+ * https://en.wikipedia.org/wiki/Bubble_sort
+ * Level: Easy
+ */
 class BubbleSort {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
